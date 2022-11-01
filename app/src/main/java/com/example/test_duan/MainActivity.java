daf50bd3ac8e50d027d75999ac742cc261927978
hung0920 <hungooso4413@gmail.com>
2022-11-01 13:16:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test_duan/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.test_duan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
