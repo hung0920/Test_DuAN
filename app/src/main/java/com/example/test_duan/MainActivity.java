5b63c33190ad042efb134d983dfe7c3edd466113
hung0920 <44435778+hung0920@users.noreply.github.com>
2022-11-01 13:48:35 +0700

    Change code dev

--- app/src/main/java/com/example/test_duan/MainActivity.java
+++ app/src/main/java/com/example/test_duan/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("ZZZZZ", "New Dev branch" );
+        Log.e("ZZZZZ", "Change from dev" );
     }
 }
