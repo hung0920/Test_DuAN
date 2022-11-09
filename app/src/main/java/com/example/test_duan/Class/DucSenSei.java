3a3d2a4c7a2bd3cc6224e282bfdb34132d6e9c0a
DucSenSei <phamxuanduc25703@gmail.com>
2022-11-09 21:54:13 +0700

    DucSenSei
    ok men

--- app/src/main/java/com/example/test_duan/Class/DucSenSei.java
+++ app/src/main/java/com/example/test_duan/Class/DucSenSei.java
@@ -4,4 +4,26 @@ public class DucSenSei {
 
     private int id;
     private String Lop;
+
+    public DucSenSei(int id, String lop) {
+        this.id = id;
+        Lop = lop;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLop() {
+        return Lop;
+    }
+
+    public void setLop(String lop) {
+        Lop = lop;
+    }
 }
+
