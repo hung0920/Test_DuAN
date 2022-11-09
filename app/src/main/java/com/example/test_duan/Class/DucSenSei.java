b58918d03a4f313cf92b9f47f5a5c8edbc3ac4a2
hung90909 <hungnvph25605@fpt.edu.vn>
2022-11-09 22:07:01 +0700

    hung
    cacs

---
+++ app/src/main/java/com/example/test_duan/Class/DucSenSei.java
@@ -0,0 +1,43 @@
+package com.example.test_duan.Class;
+
+public class DucSenSei {
+
+    private int id;
+    private String Lop;
+    private String name;
+    public DucSenSei(int id, String lop) {
+        this.id = id;
+        Lop = lop;
+    }
+
+    public DucSenSei(int id, String lop, String name) {
+        this.id = id;
+        Lop = lop;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLop() {
+        return Lop;
+    }
+
+    public void setLop(String lop) {
+        Lop = lop;
+    }
+}
+
