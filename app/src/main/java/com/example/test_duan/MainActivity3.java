5d34e3a94821adde2f0c69f7ec1f945305f80b80
Phạm Tiến Dũng <phamtiendungvn2363@gmail.com>
2022-11-11 07:49:35 +0700

    Activity3

---
+++ app/src/main/java/com/example/test_duan/MainActivity3.java
@@ -0,0 +1,15 @@
+package com.example.test_duan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        System.out.println("ABC");
+    }
+}
